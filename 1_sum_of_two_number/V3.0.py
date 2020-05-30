e34efff9b58b79fd78ba0567af178b1b9c9149d6
wowSUNBOY <809116919@qq.com>
2020-05-30 17:32:46 +0800

    change to Python3 version

--- 1_sum_of_two_number/V3.0.py
+++ 1_sum_of_two_number/V3.0.py
@@ -1,19 +1,20 @@
 # sum of two numbers by dictionary
 class solution():
       def __init__(self,ls,target):
-			self.ls=ls
-			self.target=target
+            self.ls=ls
+            self.target=target
 		
       def seqnum(self):
-		lookup={}
-		for i in range(len(self.ls)):
-			tarnum=target-self.ls[i]
-			if tarnum in lookup:
-				print('sequence numbers are',i,lookup[tarnum])
-			lookup[self.ls[i]]=i				
+        lookup={}
+        for i in range(len(self.ls)):
+            num=float(self.ls[i])
+            tarnum=target-num
+            if tarnum in lookup:
+                print('sequence numbers are',i,lookup[tarnum])
+            lookup[num]=i				
 				
 
-ls=input('Please input your number list:')
-target=input('please input your target number:')
+ls=input('Please input your number list:').split(" ")    # split the input str by ','
+target=float(input('please input your target number:'))
 sum=solution(ls,target)
 sum.seqnum() 
